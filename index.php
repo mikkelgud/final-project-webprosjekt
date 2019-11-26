a404057e64fe4fa5bc4cf728acd96e7c258f1ad3
Sigurd <sigurd@kult.design>
2019-11-26 14:15:02 +0100

    Project up

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php include("head.php"); ?>
+<?php include("menu.php"); ?>
+
+<main class="main">
+    <section class="page-header">
+        <div class="container">
+            <div class="grid">
+                <div class="col">
+                    <h1 class="u-color-white">Safe sanitary spaces to ensure young people can continue their education. </h1>
+                    <div class="button-container m-t-lg">
+                        <a class="button button--transparent" href="/donate">Donate now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="full-width-image">
+        <img src="images/person-with-painting.jpg" alt="Image of young smiling person" />
+    </section>
+
+    <section class="content-section">
+        <div class="container">
+            <div class="grid">
+                <div class="col">
+                    <h2 class="serif">Periods Affect Potential</h2>
+                    <p>In 2017, over 100 000 people in developing countries did not go to school for several days during their period. This is due to the pricing and lack of period products and unsafe hygienic conditions in their communities.</p>
+                    <q>The Orange Room creates solutions to help young people finish their education - by creating safe spaces for sanitary purposes. </q>
+                    <p>In 2017, over 100 000 people in developing countries did not go to school for several days during their period. This is due to the pricing and lack of period products and unsafe hygienic conditions in their communities.</p>
+                    
+                    <div class="button-container p-t-lg">
+                        <a class="button button--transparent-dark" href="/challenge">About the challenge</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="content-section">
+        <div class="full-width-image">
+            <img src="images/person-with-painting.jpg" alt="Image of young smiling person" />
+        </div>
+    </section>
+
+    <section class="content-section">
+        <div class="container">
+            <div class="grid">
+                <div class="col">
+                    <h2 class="serif">Our Solution: Safe spaces for sanitary purposes</h2>
+                     <img src="images/person-with-painting.jpg" alt="Image of young smiling person" />
+                    <p>In 2017, over 100 000 people in developing countries did not go to school for several days during their period. This is due to the pricing and lack of period products and unsafe hygienic conditions in their communities.</p>
+                    <q>The Orange Room creates solutions to help young people finish their education - by creating safe spaces for sanitary purposes. </q>
+                    <p>In 2017, over 100 000 people in developing countries did not go to school for several days during their period. This is due to the pricing and lack of period products and unsafe hygienic conditions in their communities.</p>
+
+                    <div class="infobox">
+                        <div class="infobox__content">
+                            <h3 class="infobox__title">Infobox</h3>
+                            <p>In 2017, over 100 000 people in developing countries did not go to school for several days during their period. </p>
+                            <a href="#">Source</a>
+                        </div>
+                    </div>
+
+                    <a href="/angola" class="linkbox">
+                        <div class="linkbox__content">
+                            <div class="linkbox__image">
+                                <img src="images/angola-person.png" />
+                            </div>
+                            <div class="linkbox__meta">
+                                <h3 class="linkbox__title">Angola</h3>
+                                <p>Read the case study</p>
+                                <svg class="arrow" width="194" height="16" viewBox="0 0 194 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M193.707 8.70712C194.098 8.3166 194.098 7.68343 193.707 7.29291L187.343 0.928949C186.953 0.538424 186.319 0.538424 185.929 0.928948C185.538 1.31947 185.538 1.95264 185.929 2.34316L191.586 8.00002L185.929 13.6569C185.538 14.0474 185.538 14.6806 185.929 15.0711C186.319 15.4616 186.953 15.4616 187.343 15.0711L193.707 8.70712ZM-8.74228e-08 9L193 9.00002L193 7.00002L8.74228e-08 7L-8.74228e-08 9Z" fill="white"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </a>
+
+                     <h2 class="serif">FAQ</h2>
+                    <details>
+                        <summary>Hei</summary>
+                        <div class="summary-content">Her kommer mer tekst</div> 
+                    </details>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</main>
+
+
+
+<?php include("footer.php"); ?>
---
+++ menu.php
@@ -0,0 +1,24 @@
+<div class="sidebar">
+    <div class="sidebar__inner">
+        <div class="logo-container">
+            <img src="images/orange-logo.png" />
+        </div>
+        <div class="menu-container">
+            <div class="single-menu-item">
+                <a class="active" href="/">Home</a>
+            </div>
+            <div class="single-menu-item">
+                <a href="/challenge">Our Challenge</a>
+            </div>
+            <div class="single-menu-item">
+                <a href="/solution">Our Solution</a>
+            </div>
+            <div class="single-menu-item">
+                <a href="/about">About</a>
+            </div>
+            <div class="single-menu-item">
+                <a href="/faq">FAQ</a>
+            </div>
+        </div>
+    </div>
+</div>
