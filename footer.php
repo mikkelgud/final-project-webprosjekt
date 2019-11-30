c2b31ab16fce2d8f5ee5705f50c9dcc2a6e6c4bf
Sigurd <sigurd@kult.design>
2019-11-30 17:44:08 +0100

    Adds footer page and some style changes

--- footer.php
+++ footer.php
@@ -1,6 +1,20 @@
 
         <footer>
-
+            <div class="container">
+                <h2>
+                    Get Involved
+                </h2>
+                <p>You can contribute to our cause by:</p>
+                <ul>
+                    <li>Help us spread this website on social media</li>
+                    <li>Become a volunteer. Sign up by sending an email to <a href="mailto:volunteer@theorangeroom.org">volunteer@theorangeroom.org</a></li>
+                    <li>Donate to us through <a href="https://www.paypal.com/">PayPal</a> with the adress <a href="mailto:donate@theorangeroom.org">donate@theorangeroom.org</a></li>
+                </ul>
+                <h3>Got any questions?</h3>
+                <p>Please send us an email at <a href="mailto:contact@theorangeroom.org">contact@theorangeroom.org</a></p>
+                <p><a href="privacy.php">Privacy Policy</a></p>
+                <p><a href="press.php">Press enquires</a></p>
+            </div>
         </footer>
         </div> <!-- /content -->
     </body>
--- international.php
+++ international.php
@@ -20,7 +20,7 @@
                    <p>The Orange Room website is currently not supported in other languages than English. If you need to view this page in your own laguage please follow these steps:</p>
                    <ol>
                        <li>Download <a href="https://www.google.com/chrome/">Google Chrome here</a></li>
-                       <li>Install Chrome in your prefered language</li>
+                       <li>Install Chrome in your preferred language</li>
                        <li>Open Chrome and visit our website agian</li>
                        <li>Press "Translate this page" on the popup, and you should see this page in your preferred language</li>
                    </ol>
